30882cf72e5bfa9b16acd1f2097863539caf4f32
boothe0 <elizabethbooth05@gmail.com>
2025-02-23 20:01:37 -0700

    First Push

---
+++ app.py
@@ -0,0 +1,42 @@
+from urllib import request
+
+from flask import Flask, render_template, request, redirect, session
+
+app = Flask(__name__)
+# Set a secret key for encrypting session data
+app.secret_key = 'my_secret_key'
+
+# dictionary to store user and password
+users = {
+    'elizabethbooth05@gmail.com': '1234',
+    'user2': 'password2'
+}
+@app.route('/')
+def home_page():
+    return render_template('home.html')
+
+
+@app.route('/handle_post', methods=['POST'])
+def handle_post():
+    if request.method == 'POST':
+        username = request.form['email']
+        password = request.form['password']
+        first_name = request.form['first_name']
+        print(username, password)
+        if username in users and users[username] == password:
+            return render_template('blogger.html', first_name=first_name)
+        else:
+            return '<h1>invalid credentials!</h1>'
+    else:
+        return render_template('login_form.html')
+
+@app.route('/login')
+def login_page():
+    return render_template('login_form.html')
+
+@app.route('/blogger')
+def blogger_main_page():
+    return render_template('blogger.html')
+
+if __name__ == '__main__':
+    app.run()
